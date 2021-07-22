6f432b2910ef810db9e6b464163aa56c982762ec
pedro14ha <80281472+pedro14ha@users.noreply.github.com>
2021-07-22 15:29:52 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from .secrets import secrets_from_environment, load_secrets
+from .secrets import load_secrets
 from .bigquery import BqClient
 from .request import get
 from .pubsub import publish_data
