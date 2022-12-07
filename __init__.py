9ee2c3fc47de0bf1bb124632c207d625257c51de
pandradepx <pedro.andrade@pxcreativelab.com>
2022-12-07 17:02:46 -0300

    add upload csv function

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from .secrets import load_secrets
+from .secrets_manager import load_secrets
 from .bigquery import BqClient
 from .request import get_api
 from .pubsub import publish_data
--- gcs.py
+++ gcs.py
@@ -19,3 +19,9 @@ def upload_json_data(bucket_name: str, blob_name: str, data: dict):
     blob = bucket.blob(blob_name)
     blob.upload_from_string(json.dumps(data, ensure_ascii=False).encode("utf-8"), timeout=540, content_type="application/json")
     return blob_name
+
+def upload_csv_data(bucket_name: str, blob_name: str, data: str):
+    bucket = storage.Client().get_bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+    blob.upload_from_string(data, timeout=540, content_type="text/csv")
+    return blob_name
